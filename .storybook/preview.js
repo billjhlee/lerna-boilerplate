a5d1ac0f11eb8d41994fbb5274f2867eb2c75b6e
billjhlee <billjhlee@gmail.com>
2021-08-03 22:37:08 +0900

    feat: bulma + storybook upgrade to webpack5
    Storybook has been upgarded to use webpack5, and bulma has been added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
+import '!style-loader!css-loader!sass-loader!@sample/web-css/src/styles/customized.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
